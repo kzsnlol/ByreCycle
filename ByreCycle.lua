290a667e8788e00f15ab6efb990189684421a94b
kzsnlol <realkzsn@gmail.com>
2026-05-18 09:15:43 +0800

    nigga

---
+++ ByreCycle.lua
@@ -0,0 +1,5 @@
+--fuck you just use ts dont skid its not even that good
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kzsnlol/ByreCycle/refs/heads/main/ByreACBypass.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kzsnlol/ByreCycle/refs/heads/main/ByreCycleRAGE.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kzsnlol/ByreCycle/refs/heads/main/ByreAim.lua"))()
